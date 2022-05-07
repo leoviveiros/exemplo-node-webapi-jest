1af6a1193c041eac1a2e59e271ce5312b73fc3e6
Leonardo Pinto <leoviveiros@gmail.com>
2022-05-07 11:42:32 -0300

    refatoracao post

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 import { createServer } from 'http';
 import { once } from 'events';
+import { randomUUID } from 'crypto';
 
 const Database = new Map();
 
 function jsonResponse(data, response) {
     return response.end(JSON.stringify(data));
+}
+
+async function postHandler(request, response) {
+    const body = JSON.parse(await once(request, 'data'));
+
+    const id = randomUUID();
+    const data = { id, ...body };
+
+    Database.set(id, data);
+
+    console.log(data);
+
+    return jsonResponse({ ok: true }, response);
 }
 
 async function handler(request, response) {
@@ -15,11 +29,7 @@ async function handler(request, response) {
     }
 
     if (method === 'POST') {
-        const body = JSON.parse(await once(request, 'data'));
-
-        console.log(body);
-
-        return jsonResponse({ ok: true }, response);
+        return postHandler(request, response);
     }
 
     if (method === 'DELETE') {
