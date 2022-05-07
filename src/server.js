b61e015497cf0fc970283eca211f5b6334281d0c
Leonardo Pinto <leoviveiros@gmail.com>
2022-05-07 11:33:38 -0300

    inclusao verbos http

--- src/server.js
+++ src/server.js
@@ -1,7 +1,30 @@
 import { createServer } from 'http';
+import { once } from 'events';
 
-async function handler(req, res) {
-    res.end('Hello World!');
+const Database = new Map();
+
+function jsonResponse(data, response) {
+    return response.end(JSON.stringify(data));
+}
+
+async function handler(request, response) {
+    const { method } = request;
+
+    if (method === 'GET') {
+        return jsonResponse([...Database.values()], response);
+    }
+
+    if (method === 'POST') {
+        const body = JSON.parse(await once(request, 'data'));
+
+        console.log(body);
+
+        return jsonResponse({ ok: true }, response);
+    }
+
+    if (method === 'DELETE') {
+        return jsonResponse({ ok: true }, response);
+    }
 }
 
 export default createServer(handler);
