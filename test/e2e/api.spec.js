406cdf91f1a1d991c6f5d108fb8f25a293d72ee4
Leonardo Pinto <leoviveiros@gmail.com>
2022-05-07 14:27:47 -0300

    test post+get+delete

--- test/e2e/api.spec.js
+++ test/e2e/api.spec.js
@@ -33,4 +33,34 @@ describe('API E2E Test Suite', () => {
 
         expect(response.text).toStrictEqual(expectedResponse);
     })
+
+    test('POST + GET + DELETE / - should save an item, retrieve it then delete it', async () => {
+        let response = await supertest(Server).post('/').send({
+            name: 'Test',
+            age: 10
+        });
+
+        const expectedResponse = JSON.stringify({ ok: true})
+
+        expect(response.text).toStrictEqual(expectedResponse);
+
+        response = await supertest(Server).get('/');
+        let data = JSON.parse(response.text);
+
+        expect(data).toBeInstanceOf(Array);
+        expect(data).toHaveLength(1);
+        expect(data[0]).toMatchObject({
+            name: 'Test',
+            age: 10
+        });
+        expect(data[0].id).toBeDefined();
+
+        await supertest(Server).delete('/');
+
+        response = await supertest(Server).get('/');
+        data = JSON.parse(response.text);
+
+        expect(data).toBeInstanceOf(Array);
+        expect(data).toHaveLength(0);
+    })
 })
